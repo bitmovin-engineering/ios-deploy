4eedbb58c7cb5f2d1fab4160fae316344d41ad46
David Steinacher <david.steinacher@bitmovin.com>
2025-12-09 11:34:45 +0100

    use url attribute

--- ios-deploy-bitmovin.rb
+++ ios-deploy-bitmovin.rb
@@ -1,8 +1,11 @@
 class IosDeployBitmovin < Formula
   desc "Install and debug iPhone apps from the command-line (Bitmovin fork)"
   homepage "https://github.com/bitmovin-engineering/ios-deploy"
-  head "https://github.com/bitmovin-engineering/ios-deploy.git", branch: "feature/add-brew-formula-file"
+  url "https://github.com/bitmovin-engineering/ios-deploy/archive/refs/heads/feature/add-brew-formula-file.tar.gz"
+  version "1.12.2-bitmovin"
+  sha256 :no_check
   license all_of: ["GPL-3.0-or-later", "BSD-3-Clause"]
+  head "https://github.com/bitmovin-engineering/ios-deploy.git", branch: "feature/add-brew-formula-file"
 
   depends_on xcode: :build
   depends_on :macos
