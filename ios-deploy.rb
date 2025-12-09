d856e670ae78b7735012de9ced6160eba34afab2
David Steinacher <david.steinacher@bitmovin.com>
2025-12-09 11:30:57 +0100

    add homebrew formula file

---
+++ ios-deploy.rb
@@ -0,0 +1,21 @@
+class IosDeploy < Formula
+  desc "Install and debug iPhone apps from the command-line (Bitmovin fork)"
+  homepage "https://github.com/bitmovin-engineering/ios-deploy"
+  head "https://github.com/bitmovin-engineering/ios-deploy.git", branch: "feature/add-brew-formula-file"
+  license all_of: ["GPL-3.0-or-later", "BSD-3-Clause"]
+
+  depends_on xcode: :build
+  depends_on :macos
+
+  def install
+    xcodebuild "-configuration", "Release",
+               "SYMROOT=build",
+               "-arch", Hardware::CPU.arch
+
+    bin.install "build/Release/ios-deploy"
+  end
+
+  test do
+    system bin/"ios-deploy", "-V"
+  end
+end
