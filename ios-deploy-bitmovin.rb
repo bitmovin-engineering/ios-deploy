b0414c43612099dd9a181882343d10d568b15431
David Steinacher <david.steinacher@bitmovin.com>
2025-12-09 13:03:25 +0100

    remove url from formula file

--- ios-deploy-bitmovin.rb
+++ ios-deploy-bitmovin.rb
@@ -1,9 +1,6 @@
 class IosDeployBitmovin < Formula
   desc "Install and debug iPhone apps from the command-line (Bitmovin fork)"
   homepage "https://github.com/bitmovin-engineering/ios-deploy"
-  url "https://github.com/bitmovin-engineering/ios-deploy/archive/refs/heads/feature/add-brew-formula-file.tar.gz"
-  version "1.12.2-bitmovin"
-  sha256 :no_check
   license all_of: ["GPL-3.0-or-later", "BSD-3-Clause"]
   head "https://github.com/bitmovin-engineering/ios-deploy.git", branch: "feature/add-brew-formula-file"
 
