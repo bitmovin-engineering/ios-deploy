ebc8be1f5de3147e350ac9ad6c1a0eb63fb86056
David Steinacher <david.steinacher@bitmovin.com>
2025-12-09 13:19:44 +0100

    use ssh

--- ios-deploy-bitmovin.rb
+++ ios-deploy-bitmovin.rb
@@ -2,7 +2,7 @@ class IosDeployBitmovin < Formula
   desc "Install and debug iPhone apps from the command-line (Bitmovin fork)"
   homepage "https://github.com/bitmovin-engineering/ios-deploy"
   license all_of: ["GPL-3.0-or-later", "BSD-3-Clause"]
-  head "https://github.com/bitmovin-engineering/ios-deploy.git", branch: "feature/add-brew-formula-file"
+  head "ssh://git@github.com/bitmovin-engineering/ios-deploy.git"
 
   depends_on xcode: :build
   depends_on :macos
